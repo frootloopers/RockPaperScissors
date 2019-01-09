80aa925ddd465e6ae2ada3a7f2a922e7ad7292c3
redti <redti@10.242.95.127>
2019-01-09 14:34:40 -0500

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

--- src/Foundation/Map.java
+++ src/Foundation/Map.java
@@ -21,7 +21,7 @@ public class Map {
     private ArrayList<Bullet> Bullets;
     private int teams;
 
-    public Map(int teams, int harvestables) {
+    public Map(int harvestables) {
         this.teams = teams;
         Entities = new Entity[teams * 4];
         Harvestables = new Harvestable[harvestables];
@@ -29,7 +29,7 @@ public class Map {
     }
 
     /**
-     * Returns a list of entities in a team
+     * Returns a list of harvestables
      *
      * @param team
      * @return
@@ -39,7 +39,7 @@ public class Map {
     }
 
     /**
-     * Returns a list of all teams
+     * Returns a list of all AI objects
      *
      * @return
      */
@@ -48,7 +48,7 @@ public class Map {
     }
 
     /**
-     * Returns a list of all teams
+     * Returns a list of all bullets
      *
      * @return
      */
