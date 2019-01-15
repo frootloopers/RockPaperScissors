6197b24f4a36e73bcb5aabd6c9ded9409e7de6d1
redti <redti@10.242.95.127>
2019-01-15 14:11:29 -0500

    Merge origin/master
    Conflicts:
    	src/Foundation/Map.java

--- src/GUI/TestingPanel.java
+++ src/GUI/TestingPanel.java
@@ -29,9 +29,9 @@ public class TestingPanel extends javax.swing.JPanel {
     Drone d;
     Ship s;
 
-    double zoom;
-    int offsetX;
-    int offsetY;
+    double zoom = 1;
+    int offsetX = 0;
+    int offsetY = 0;
 
     /**
      * Creates new form GamePanel
@@ -97,7 +97,7 @@ public class TestingPanel extends javax.swing.JPanel {
     public void paintComponent(Graphics g) {
         Image img = Toolkit.getDefaultToolkit().getImage("src/spaceRazeBackground1.png");
         //a.setThrustF(5);
-        g.drawImage(img, 0, 0, this.getWidth() , this.getHeight(), this);
+        g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
 
         updateGraphics(g, GameBoard);
 
