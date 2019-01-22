a52de6150659c38fa56c77dd884bfe2c263748f8
redti <redti@10.242.95.127>
2019-01-22 13:29:01 -0500

    Merge origin/master
    Conflicts:
    	src/Entities/Map.java

--- src/Entities/Controllable.java
+++ src/Entities/Controllable.java
@@ -76,11 +76,12 @@ public abstract class Controllable extends Movable {
         g.setColor(Color.MAGENTA);
         //center of the controllable where everything is drawn from
         Point temp = new Point((int) ((getPos().getX() + offsetX) * scale), (int) ((getPos().getY() + offsetY) * scale));
-        g.fillRect(temp.x, temp.y, 60, 24);
+        g.fillRect(temp.x, temp.y, 60, 36);
         g.setColor(Color.BLACK);
-        g.drawRect(temp.x, temp.y, 60, 24);
+        g.drawRect(temp.x, temp.y, 60, 36);
         g.drawString(Math.round(getPos().x) + ", " + Math.round(getPos().y), temp.x + 3, temp.y + 11);
-        g.drawString(Integer.toString(storage) + " res", temp.x + 3, temp.y + 22);
+        g.drawString(Double.toString(faceAngle) + " deg", temp.x + 3, temp.y + 22);
+        g.drawString(Integer.toString(storage) + " res", temp.x + 3, temp.y + 33);
     }
 
     /**
